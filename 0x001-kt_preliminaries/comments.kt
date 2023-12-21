e86a936264f08fd7e0ac8405ac9ee1cd0cb86f76
Churchil-Oduor <churchilokechoduor@gmail.com>
2023-12-21 23:04:18 +0300

    Added the variables and comments file

---
+++ 0x001-kt_preliminaries/comments.kt
@@ -0,0 +1,9 @@
+fun main()
+{
+	// single line comment
+
+	/**
+		this is a multline comment
+	**/
+
+}
---
+++ 0x001-kt_preliminaries/variables.kt
@@ -0,0 +1,7 @@
+fun main()
+{
+    var name: String = "Churchil"
+    var age: Int = 10
+
+    println("Hello ! my name is ${name}, I am ${age} years old")
+}
